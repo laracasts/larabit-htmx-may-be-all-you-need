cf8fe2b0e497b8ad30690981ce4c2f3ed00ae5f4
Jeremy McPeak <jwmcpeak@gmail.com>
2024-01-08 19:12:49 -0600

    more prep

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>HTMX</title>
+        <title>Larabits: HTMX</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -12,7 +12,6 @@
 
         <!-- Styles -->
         <script src="https://cdn.tailwindcss.com"></script>
-        <script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
     </head>
     <body class="antialiased bg-slate-700">
         <div class="flex justify-center px-10 py-10 w-full">
